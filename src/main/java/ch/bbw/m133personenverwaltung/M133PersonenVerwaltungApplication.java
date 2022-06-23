ddf21347395bcc5de420767029891ef8e5ae40eb
rra <gianluca.ferrara@ti8m.ch>
2022-06-23 09:54:46 +0200

    first commit

---
+++ src/main/java/ch/bbw/m133personenverwaltung/M133PersonenVerwaltungApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.m133personenverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M133PersonenVerwaltungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M133PersonenVerwaltungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/m133personenverwaltung/M133PersonenVerwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.m133personenverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M133PersonenVerwaltungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
