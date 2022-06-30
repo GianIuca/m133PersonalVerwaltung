3174cfdf4455ba80f3c9412e96ffaedf91cb80d4
rra <gianluca.ferrara@ti8m.ch>
2022-06-30 10:35:06 +0200

    added MainController

---
+++ src/main/java/ch/bbw/m133personenverwaltung/MainController.java
@@ -0,0 +1,12 @@
+package ch.bbw.m133personenverwaltung;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
