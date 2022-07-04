36780685019f8dda3c332a5d2a0ec07dc8d2d964
rra <gianluca.ferrara@ti8m.ch>
2022-07-04 20:44:01 +0200

    added PersonRepository

---
+++ src/main/java/ch/bbw/m133personenverwaltung/model/PersonRepository.java
@@ -0,0 +1,6 @@
+package ch.bbw.m133personenverwaltung.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
